c5961d597e712566c7dde4d5dc5c59e0e4455878
Ashwin-1020 <ashwinramar03@gmail.com>
2025-06-02 14:34:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (98.67%) - LeetSync

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def minCostToMoveChips(self, position):
+       even = sum(p % 2 == 0 for p in position)
+       odd = len(position) - even
+       return min(even, odd)
+        
