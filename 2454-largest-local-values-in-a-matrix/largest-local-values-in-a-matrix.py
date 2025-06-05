3aa8421c52d91a845098ec203337628100f7c3be
Ashwin-1020 <ashwinramar03@gmail.com>
2025-06-05 10:17:05 +0530

    Time: 55 ms (52.38%) | Memory: 13 MB (61.90%) - LeetSync

---
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def largestLocal(self, grid):
+        n = len(grid)-2
+        ans = [[0]*n for _ in range(n)]
+
+        for i,j in product(range(n),range(n)):
+            ans[i][j] = max(grid[I][J] for I,J in
+                            product(range(i,i+3),range(j,j+3)))
+
+        return  ans
+        
